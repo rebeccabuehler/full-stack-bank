3750d5a565155c0641fd873c134c723ac1f58cb4
Rebecca Buehler <you@example.com>
2022-11-29 16:12:46 -0800

    adding my files

---
+++ fullstack bad bank/public/navbar.js
@@ -0,0 +1,34 @@
+function NavBar(){
+  return(
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/home">BadBank</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+        <li className="nav-item">
+            <Link className="nav-link" to="/createaccount">Create Account</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/deposit">Deposit</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/withdraw">Withdraw</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/balance">Balance</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/alldata">AllData</Link>
+          </li>          
+        </ul>
+      </div>
+    </nav>
+
+  );
+}
