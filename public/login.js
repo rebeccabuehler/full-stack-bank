3624bfdf5ce8680f8df048d3b75b52c4845a1c59
Rebecca Buehler <you@example.com>
2023-01-06 12:41:40 -0800

    updating name of bank through app

--- public/login.js
+++ public/login.js
@@ -1,3 +1,5 @@
+const { useResolvedPath } = require("react-router");
+
 function Login(){
   const [show, setShow]     = React.useState(true);
   const [status, setStatus] = React.useState('');    
@@ -37,7 +39,7 @@ function LoginForm(props){
             const data = JSON.parse(text);
             props.setStatus('');
             props.setShow(false);
-            console.log('JSON:', data);
+            console.log('JSON:', data.name);
         } catch(err) {
             props.setStatus(text)
             console.log('err:', text);
@@ -45,7 +47,8 @@ function LoginForm(props){
     });
   }
 
-  return (<>
+  return (
+  <>
 
     Email<br/>
     <input type="input" 
--- public/navbar.js
+++ public/navbar.js
@@ -1,34 +1,57 @@
-function NavBar(){
-  return(
-
+function NavBar() {
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/home">BadBank</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <Link className="navbar-brand" to="/">
+        Trust Bank
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-        <li className="nav-item">
-            <Link className="nav-link" to="/createaccount">Create Account</Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/createaccount">
+              Create Account
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/login">Login</Link>
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/deposit">Deposit</Link>
+            <Link className="nav-link" to="/deposit">
+              Deposit
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/withdraw">Withdraw</Link>
+            <Link className="nav-link" to="/withdraw">
+              Withdraw
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/balance">Balance</Link>
+            <Link className="nav-link" to="/balance">
+              Balance
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/alldata">AllData</Link>
-          </li>          
+            <Link className="nav-link" to="/alldata">
+              AllData
+            </Link>
+          </li>
+        </ul>
+        <ul className="ml-auto">
+          <span className="nav-item">Welcome User!</span>
         </ul>
       </div>
     </nav>
-
   );
 }
